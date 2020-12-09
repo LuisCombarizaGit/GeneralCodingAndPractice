6552a49d1ba29f9e709356432befff0ede859fa2
Luis Combariza <luis_combariza@outlook.com>
2020-12-08 19:44:45 -0800

    updated javaScript practice file

---
+++ webprogramming/app.js
@@ -0,0 +1,8 @@
+let my_element = document.createElement('h1');
+let my_span = document.createElement('span');
+
+my_span.innerText = "you mother"
+
+my_element.appendChild(my_span);
+
+document.body.appendChild(my_element);
