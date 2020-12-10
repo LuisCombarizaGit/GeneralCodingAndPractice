31e414669bacfb48cda3d8f9f0cebb5d7c5bb29b
Luis Combariza <luis_combariza@outlook.com>
2020-12-09 20:11:20 -0700

    updated practice html and JavaScript file

--- webprogramming/app.js
+++ webprogramming/app.js
@@ -1,3 +1,35 @@
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Paragraph changed.";
-}
+/* The Difference Between Undefined and Null
+
+typeof undefined -------> undefined
+typeof null --------> object
+
+null === undefined ------> false
+null == undefined -----> true 
+
+
+When a JavaCript variable is declared with the keyword "new",
+the variable is created as an object
+
+Common HTML Events :
+  onchange - An HTML element has been changed
+  onclick - The user clicks an HTML element
+  onmouseover - The user moves the mouse over an HTML element
+  onmouseout - The user moves the mouse away from an HTML element
+  onkeydown - The user pushes a keyboard key
+  onload - The browser has finished loading the page
+
+
+  NaN - Reserved word indidcating that a number is not a legal number
+  isNaN - global JavaScript function to find out if a value is a number
+  typeof(NaN) = returns number.
+
+
+  infinity key world essetially defines infiinity
+  typeof Infinity is number
+
+
+Arrays in JavaScript are Objects. But is best to descrive them as
+arrays due to the way the indexing works.
+
+
+*/
