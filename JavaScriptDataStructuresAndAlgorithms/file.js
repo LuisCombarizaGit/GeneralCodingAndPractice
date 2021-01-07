e89916a9a0f29b2f9bf5ba2c736b0786dc3dc6c8
Luis Combariza <luis_combariza@outlook.com>
2021-01-06 20:02:00 -0700

    updated JavaScript file

--- JavaScriptDataStructuresAndAlgorithms/file.js
+++ JavaScriptDataStructuresAndAlgorithms/file.js
@@ -230,16 +230,21 @@ class BST {
   }
   add(data) {
     const node = this.root;
+    // If the root node is null then make node the root node:
     if (node === null) {
       this.root = new Node(data);
       return;
-    } else {
+    }
+    // Find where to put the node based on the value of the node:
+    else {
       const searchTree = function (node) {
+        // If data is less then root than put it on left side of tree
         if (data < node.data) {
           if (node.left === null) {
             node.left = new Node(data);
             return;
           } else if (node.left !== null) {
+            // Recursive function to move down the tree and check
             return searchTree(node.left);
           }
         } else if (data > node.data) {
@@ -247,15 +252,19 @@ class BST {
             node.right = new Node(data);
             return;
           } else if (node.right !== null) {
+            // Recursive function to move down the tree and check
             return searchTree(node.right);
           }
         } else {
+          // The value of the node is already present in the tree : return null
           return null;
         }
       };
       return searchTree(node);
     }
   }
+  // Traverse down the left side of the tree untill you reach the last node which
+  // is the lowest value in the tree.
   findMin() {
     let current = this.root;
     while (current.left !== null) {
@@ -263,6 +272,8 @@ class BST {
     }
     return current.data;
   }
+  // Traverse down the right side of the tree untill you reach the last node which
+  // is the highest value in the tree.
   findMax() {
     let current = this.root;
     while (current.right !== null) {
@@ -270,6 +281,7 @@ class BST {
     }
     return current.data;
   }
+
   find(data) {
     let current = this.root;
     while (current.data !== data) {
@@ -284,6 +296,9 @@ class BST {
     }
     return current;
   }
+
+  // Traverse left and right starting from the root node untill you find the data
+  // if the data is not found then return false
   isPresent(data) {
     let current = this.root;
     while (current) {
@@ -298,6 +313,7 @@ class BST {
     }
     return false;
   }
+
   remove(data) {
     const removeNode = function (node, data) {
       if (node == null) {
@@ -334,119 +350,22 @@ class BST {
     };
     this.root = removeNode(this.root, data);
   }
-  isBalanced() {
-    return this.findMinHeight() >= this.findMaxHeight() - 1;
-  }
-  findMinHeight(node = this.root) {
-    if (node == null) {
-      return -1;
-    }
-    let left = this.findMinHeight(node.left);
-    let right = this.findMinHeight(node.right);
-    if (left < right) {
-      return left + 1;
-    } else {
-      return right + 1;
-    }
-  }
-  findMaxHeight(node = this.root) {
-    if (node == null) {
-      return -1;
-    }
-    let left = this.findMaxHeight(node.left);
-    let right = this.findMaxHeight(node.right);
-    if (left > right) {
-      return left + 1;
-    } else {
-      return right + 1;
-    }
-  }
-  inOrder() {
-    if (this.root == null) {
-      return null;
-    } else {
-      var result = new Array();
-      function traverseInOrder(node) {
-        node.left && traverseInOrder(node.left);
-        result.push(node.data);
-        node.right && traverseInOrder(node.right);
-      }
-      traverseInOrder(this.root);
-      return result;
-    }
-  }
-  preOrder() {
-    if (this.root == null) {
-      return null;
-    } else {
-      var result = new Array();
-      function traversePreOrder(node) {
-        result.push(node.data);
-        node.left && traversePreOrder(node.left);
-        node.right && traversePreOrder(node.right);
-      }
-      traversePreOrder(this.root);
-      return result;
-    }
-  }
-  postOrder() {
-    if (this.root == null) {
-      return null;
-    } else {
-      var result = new Array();
-      function traversePostOrder(node) {
-        node.left && traversePostOrder(node.left);
-        node.right && traversePostOrder(node.right);
-        result.push(node.data);
-      }
-      traversePostOrder(this.root);
-      return result;
-    }
-  }
-
-  levelOrder() {
-    let result = [];
-    let Q = [];
-    if (this.root != null) {
-      Q.push(this.root);
-      while (Q.length > 0) {
-        let node = Q.shift();
-        result.push(node.data);
-        if (node.left != null) {
-          Q.push(node.left);
-        }
-        if (node.right != null) {
-          Q.push(node.right);
-        }
-      }
-      return result;
-    } else {
-      return null;
-    }
-  }
 }
 
 const bst = new BST();
 
-bst.add(9);
 bst.add(4);
-bst.add(17);
-bst.add(3);
+bst.add(2);
 bst.add(6);
-bst.add(22);
+bst.add(1);
+bst.add(3);
 bst.add(5);
 bst.add(7);
-bst.add(20);
-
-console.log(bst.findMinHeight());
-console.log(bst.findMaxHeight());
-console.log(bst.isBalanced());
-bst.add(10);
-console.log(bst.findMinHeight());
-console.log(bst.findMaxHeight());
-console.log(bst.isBalanced());
-console.log("inOrder: " + bst.inOrder());
-console.log("preOrder: " + bst.preOrder());
-console.log("postOrder: " + bst.postOrder());
-
-console.log("levelOrder: " + bst.levelOrder());
+bst.remove(4);
+console.log(bst.findMin());
+console.log(bst.findMin());
+console.log(bst.findMin());
+console.log(bst.isPresent(4));
+console.log(bst.findMax());
+bst.remove(7);
+console.log(bst.findMax());
