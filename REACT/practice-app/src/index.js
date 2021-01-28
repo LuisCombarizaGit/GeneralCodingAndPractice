f8360d2fb55a360287f55bbf88cb8a42f4fadb5f
Luis Combariza <luis_combariza@outlook.com>
2021-01-28 13:19:42 -0700

    updated React file

--- REACT/practice-app/src/index.js
+++ REACT/practice-app/src/index.js
@@ -1,50 +1,23 @@
-import React from "react"; // ES6 module dependency import -JavaScript-
-import ReactDom from "react-dom";
+// ES6 module dependency import : creates a new variable called REACT with the contents of the react module
+import React from "react"; //module name installed by npm
+import ReactDOM from "react-dom";
 
-// CSS import
-import "./index.css";
-
-// Setup vars
-const books = [
-  {
-    img: "https://m.media-amazon.com/images/I/41lnWPdpvTL._SY346_.jpg",
-    title: "I love you to the Moon and Back",
-    author: " LLLisa Pasold",
-  },
-  {
-    img: "https://m.media-amazon.com/images/I/41lnWPdpvTL._SY346_.jpg",
-    title: "I love you to the Moon and Back",
-    author: " PPPisa Pasold",
-  },
-];
-
-function BookList() {
+function HelloWorld() {
   return (
-    <section className="booklist">
-      {books.map((book, index) => {
-        return <Book Key={book.id} {...book}></Book>;
-      })}
-    </section>
+    <div>
+      {" "}
+      <Hello />
+      <World />!
+    </div>
   );
 }
 
-const Book = ({ img, title, author }) => {
-  // Attribute, eventHandler
-  // onClick, onMouseOver
+function Hello() {
+  return <span>Hello</span>;
+}
 
-  const clickHandler = () => {
-    alert("Hello World");
-  };
-  return (
-    <article className="book">
-      <img src={img} alt="" />
-      <h1>{title}</h1>
-      <h4>{author} </h4>
-      <button typre="button" Onclick={clickHandler}>
-        Reference example
-      </button>
-    </article>
-  );
-};
+function World() {
+  return <span>World</span>;
+}
 
-ReactDom.render(<BookList />, document.getElementById("root"));
+ReactDOM.render(<HelloWorld />, document.querySelector("#root"));
