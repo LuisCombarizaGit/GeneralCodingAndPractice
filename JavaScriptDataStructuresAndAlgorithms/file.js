63e6f8d521ad367dbbc10b50639368e458e0ce83
Luis Combariza <luiscombariza@Luiss-MacBook-Air.local>
2021-04-09 20:31:16 -0600

    updated file.js

--- JavaScriptDataStructuresAndAlgorithms/file.js
+++ JavaScriptDataStructuresAndAlgorithms/file.js
@@ -81,101 +81,142 @@
 //  =============== No especific order ============= //
 //  ==== Used to check for the presence of an item == //
 
-function mySet(){
+// function mySet(){
 
-    // The array collection will hold the set
-    var collection = [];
+//     // The array collection will hold the set
+//     var collection = [];
 
-    // This method will check for the presence of an element and return true or false
-    this.has = function(element){
-        return(collection.indexOf(element) !== -1 );
-    }
+//     // This method will check for the presence of an element and return true or false
+//     this.has = function(element){
+//         return(collection.indexOf(element) !== -1 );
+//     }
 
-    // This method will return all the values in the set
-    this.values = function(){
-        return collection;
-    }
+//     // This method will return all the values in the set
+//     this.values = function(){
+//         return collection;
+//     }
 
-    // This method will add an element to the set
-    this.add = function(element){
-        if(!this.has(element)){
-            collection.push(element);
-            return true;
-        }
-        return false;
+//     // This method will add an element to the set
+//     this.add = function(element){
+//         if(!this.has(element)){
+//             collection.push(element);
+//             return true;
+//         }
+//         return false;
+//     };
+
+//     // This method will remove an element from a set
+//     this.remove = function(element){
+//         if(this.has(element)){
+//             index = collection.indexOf(element);
+//             collection.splice(index,1); // remove one item at [index]
+//             return true;
+//         }
+//     }
+
+//     // This method will return the size of the colleciton
+//     this.size = function(){
+//         return collection.length;
+//     }
+
+//     // This method will return the union of two sets
+//     this.union = function(otherSet){
+//         var unionSet = new mySet();
+//         var firstSet = this.values();
+//         var secondSet = otherSet.values();
+//         firstSet.forEach(function(e){
+//             unionSet.add(e);
+//         });
+//         secondSet.forEach(function(e){
+//             unionSet.add(e);
+//         });
+//         return unionSet;
+//     };
+
+//     // This method will return the intersection of two sets as a new set
+//     this.intersection = function(otherSet){
+//         var intersectionSet = new mySet();
+//         var firstSet = this.values();
+//         firstSet.forEach(function(e){
+//             if(otherSet.has(e)){
+//                 intersectionSet.add(e);
+//             }
+//         });
+//         return intersectionSet;
+//     }
+
+
+//     // This method will return the difference of two sets as a new set
+//     this.difference = function(otherSet){
+//         var differenceSet = new mySet();
+//         var firstSet = this.values();
+//         firstSet.forEach(function(e){
+//             if(!otherSet.has(e)){
+//                 differentSet.add(e);
+//             }
+//         })
+//         return differenceSet();
+//     }
+
+//     // This method will test if the set is a subset of a different set
+//     this.subset = function(otherSet){
+//         var firstSet = this.values();
+//         return firstSet.every(function(value){
+//             return otherSet.has(value);
+//         });
+//     }
+// }
+
+
+// var setA = new mySet();
+// var setB = new mySet();
+// setA.add('a');
+// setA.add('a');
+// setB.add('b');
+// setB.add('c');
+// setB.add('a');
+// setB.add('d');
+// console.log(setA.values());
+// console.log(setB.values());
+// console.log(setA.subset(setB));
+
+
+///  ===================  QUEUE ======================= ///
+///  ============== FIRST IN FIRST OUT =============== ///
+
+function Queue() {
+    collection =[];
+    this.print = function(){
+        console.log(collection);
     };
 
-    // This method will remove an element from a set
-    this.remove = function(element){
-        if(this.has(element)){
-            index = collection.indexOf(element);
-            collection.splice(index,1); // remove one item at [index]
-            return true;
-        }
-    }
+    this.enqueue = function(element){
+        collection.push(element);
+    };
 
-    // This method will return the size of the colleciton
+    this.dequeue = function(){
+        return collection.shift(); // removes first element of array and return element
+    };
+
+    this. front = function(){
+        return collection[0];
+    };
+
     this.size = function(){
-        return collection.length;
+        return collection.lenth;
     }
 
-    // This method will return the union of two sets
-    this.union = function(otherSet){
-        var unionSet = new mySet();
-        var firstSet = this.values();
-        var secondSet = otherSet.values();
-        firstSet.forEach(function(e){
-            unionSet.add(e);
-        });
-        secondSet.forEach(function(e){
-            unionSet.add(e);
-        });
-        return unionSet;
-    };
-
-    // This method will return the intersection of two sets as a new set
-    this.intersection = function(otherSet){
-        var intersectionSet = new mySet();
-        var firstSet = this.values();
-        firstSet.forEach(function(e){
-            if(otherSet.has(e)){
-                intersectionSet.add(e);
-            }
-        });
-        return intersectionSet;
+    this.isEmpty = function(){
+        return(collection.length === 0);
     }
 
-
-    // This method will return the difference of two sets as a new set
-    this.difference = function(otherSet){
-        var differenceSet = new mySet();
-        var firstSet = this.values();
-        firstSet.forEach(function(e){
-            if(!otherSet.has(e)){
-                differentSet.add(e);
-            }
-        })
-        return differenceSet();
-    }
-
-    // This method will test if the set is a subset of a different set
-    this.subset = function(otherSet){
-        var firstSet = this.values();
-        return firstSet.every(function(value){
-            return otherSet.has(value);
-        });
-    }
 }
 
-
-var setA = new mySet();
-var setB = new mySet();
-setA.add('a');
-setA.add('a');
-setB.add('b');
-setB.add('c');
-setB.add('a');
-setB.add('d');
-console.log(setA.values());
-console.log(setB.values());
-console.log(setA.subset(setB));
+var q  = new Queue();
+q.enqueue('a');
+q.enqueue('b');
+q.enqueue('c');
+q.print();
+q.dequeue();
+q.front();
+q.print();
