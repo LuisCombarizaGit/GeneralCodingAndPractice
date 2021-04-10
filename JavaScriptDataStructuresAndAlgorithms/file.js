8329395d2e06940025492b017cdd091e4f457066
Luis Combariza <luis_combariza@outlook.com>
2021-04-09 21:17:35 -0600

    updated file.js

--- JavaScriptDataStructuresAndAlgorithms/file.js
+++ JavaScriptDataStructuresAndAlgorithms/file.js
@@ -184,39 +184,145 @@
 ///  ===================  QUEUE ======================= ///
 ///  ============== FIRST IN FIRST OUT =============== ///
 
-function Queue() {
-    collection =[];
-    this.print = function(){
-        console.log(collection);
-    };
+// function Queue() {
+//     collection =[];
+//     this.print = function(){
+//         console.log(collection);
+//     };
 
-    this.enqueue = function(element){
-        collection.push(element);
-    };
+//     this.enqueue = function(element){
+//         collection.push(element);
+//     };
 
-    this.dequeue = function(){
-        return collection.shift(); // removes first element of array and return element
-    };
+//     this.dequeue = function(){
+//         return collection.shift(); // removes first element of array and return element
+//     };
 
-    this. front = function(){
-        return collection[0];
-    };
+//     this. front = function(){
+//         return collection[0];
+//     };
 
-    this.size = function(){
-        return collection.lenth;
+//     this.size = function(){
+//         return collection.lenth;
+//     }
+
+//     this.isEmpty = function(){
+//         return(collection.length === 0);
+//     }
+
+// }
+
+// var q  = new Queue();
+// q.enqueue('a');
+// q.enqueue('b');
+// q.enqueue('c');
+// q.print();
+// q.dequeue();
+// console.log(q.front());
+// q.print();
+
+// ==============  Priority queue ============ // 
+// function PriorityQueue() {
+//     var collection = [];
+//     this.printCollection = function(){
+//         console.log(collection);
+//     };
+
+//     this.enqueue = function (element){
+//         if(this.isEmpty()){
+//             collection.push(element);
+//         }else{
+//             var added = false;
+//             for(var i=0; i<collection.length; i++){
+//                 if(element[1] < collection[i][1]){
+//                     collection.splice(i,0,element);
+//                     added = true;
+//                     break;
+//                 }
+//             }
+//             if(!added){
+//                 collection.push(element);
+//             }
+//         }
+
+//     };
+
+//     this.dequeue = function(){
+//         var value = collection.shift();
+//         return value[0];
+//     };
+
+//     this.front = function(){
+//         return collection[0];
+//     };
+    
+//     this.size = function(){
+//         return collection.length;
+//     };
+
+//     this.isEmpty = function(){
+//         return(collection.length === 0);
+//     }
+
+// }
+
+
+// var pq = new PriorityQueue();
+// pq.enqueue(['Luis', 2]);
+// pq.enqueue(['Quincy Larson',3]);
+// pq.enqueue(['Juan C',1]);
+// pq.enqueue(['SomeoneElse',1])
+// pq.printCollection();
+// pq.dequeue();
+// pq.front();
+// pq.printCollection();
+
+
+
+
+// ==================== Binary Serach Tree ================ //
+
+class Node { 
+    constructor(data, left = null, right = null ){
+        this.data = data;
+        this.left = left;
+        this.right = right;
     }
-
-    this.isEmpty = function(){
-        return(collection.length === 0);
-    }
-
 }
 
-var q  = new Queue();
-q.enqueue('a');
-q.enqueue('b');
-q.enqueue('c');
-q.print();
-q.dequeue();
-q.front();
-q.print();
+class BST {
+    constructor(){
+        this.root = null;
+    }
+    add(data){
+        const node = this.root;
+        if (node === null){
+            this.root = new Node(data);
+            return;
+        }
+        else {
+            const searchTree = function(node){
+                if(data < node.data){
+                    if(node.left === null){
+                        node.left = new Node(data);
+                        return;
+                    } else if(node.left !== null){
+                        return searchTree(node.left)
+                    }
+                } else if (node.left !== null){
+                    return searchTree(node.left);
+                } else if (data > node.data){
+                    if (node.right === null){
+                        node.right = new Node(data);
+                        return;
+                    } else if (node.right !== null){
+                        return searchTree(node.right);
+                    }
+                } else {
+                    return null;
+                }
+            };
+            return searchTree(node);
+        }
+    }
+}
