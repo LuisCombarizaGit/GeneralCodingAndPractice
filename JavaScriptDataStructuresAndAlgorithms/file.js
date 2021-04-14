d39f3c2545496b13dfe407466a0c67595da72d54
Luis Combariza <luis_combariza@outlook.com>
2021-04-13 21:10:18 -0600

    Updated file.js

--- JavaScriptDataStructuresAndAlgorithms/file.js
+++ JavaScriptDataStructuresAndAlgorithms/file.js
@@ -294,6 +294,7 @@ class BST {
     constructor(){
         this.root = null;
     }
+    // This is how you define a function when dealing with classess
     add(data){
         const node = this.root;
         if (node === null){
@@ -301,6 +302,7 @@ class BST {
             return;
         }
         else {
+            // Recursive function to check the nodes in the tree
             const searchTree = function(node){
                 if(data < node.data){
                     if(node.left === null){
@@ -309,8 +311,6 @@ class BST {
                     } else if(node.left !== null){
                         return searchTree(node.left)
                     }
-                } else if (node.left !== null){
-                    return searchTree(node.left);
                 } else if (data > node.data){
                     if (node.right === null){
                         node.right = new Node(data);
@@ -325,4 +325,96 @@ class BST {
             return searchTree(node);
         }
     }
+
+    findMin(){
+        let current = this.root;
+        while(current.left !== null){
+            current = current.left;
+        }
+        return current.data;
+    }
+
+    findMax(){
+        let current = this.root;
+        while(current.right !== null){
+            current = current.right;
+        }
+        return current.data;
+    }
+
+    find(data){
+        let current = this.root;
+        while(current.data !== data){
+            if(data < current.data){
+                current = current.left;
+            }else{
+            current = current.right;
+        }
+        if(current === null){
+            return null;
+            }
+         }
+    }
+
+
+    isPresent(data){
+        let current = this.root;
+        while(current){
+            if(data === current.data){
+                return true;
+            }
+            if (data < current.data){
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+
+        return false;
+    }
+
+    remove(data){
+        const removeNode = function(node, data){
+            if(node == null){
+                return null;
+            }
+            if(data == node.data){
+                // node has no children
+                if(node.left == null && node.right == null){
+                    return null;
+                }
+                // node has no left child
+                if(node.left == null){
+                    return node.right;
+                }
+                // node has no right child
+                if(node.right == null){
+                    return node.left;
+                }
+
+                // node has two children
+                var tempNode = node.right;
+                while(tempNode.left !== null){
+                    tempNode = tempNode.left;
+                }
+
+                node.data = tempNode.data;
+                node.right = removeNode(node.right, tempNode.data);
+                return node;
+            }   else if(data < node.data){
+                node.left = removeNode(node.left, data);
+                return node;
+            }   else {
+                node.right = removeNode(node.right, data);
+                return node;
+            }
+
+            this.root = removeNode(this.root, data);
+
+        }
+    }
+
+
 }
+
+
